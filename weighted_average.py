f6d31b53206468f4415fd401384fbdf2fee438a5
Franz Garcia <franzcgarcia@gmail.com>
2024-04-14 17:50:34 -0700

    RATIO now supported.

--- weighted_average.py
+++ weighted_average.py
@@ -1,9 +1,11 @@
+RATIO = (2.1,)
+
 def weighted_average(xs):
     num = 0
     den = 0
     for p in range(len(xs)):
-        num += xs[p] * 2 ** p
-        den += 2 ** p
+        num += xs[p] * RATIO[0] ** p
+        den += RATIO[0] ** p
     return 1.0 * num / den
 
 def main():
